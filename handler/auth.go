a5ced3d0ed2937dc9de85527198538d5de14288a
bangsyir <bangsyir@gmail.com>
2023-01-04 01:05:28 +0800

    add login handler

--- handler/auth.go
+++ handler/auth.go
@@ -2,12 +2,23 @@ package handler
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/bangsyir/notes/database"
 	"github.com/bangsyir/notes/helper"
 	"github.com/bangsyir/notes/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type User struct {
+	Name     string
+	Username string
+	Email    string
+	Password string
+}
 
 func Register(c *fiber.Ctx) error {
 	// get data from request body
@@ -35,4 +46,39 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&newUser)
 	// response data
 	return c.JSON(newUser)
+}
+
+func Login(c *fiber.Ctx) error {
+	var login struct {
+		Email    string
+		Password string
+	}
+	if err := c.BodyParser(&login); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+	user := models.User{}
+	database.DB.Where("email = ?", login.Email).First(&user)
+
+	if user.ID == 0 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "user not found"})
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(login.Password))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid email or password"})
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Failed to create token"})
+	}
+
+	c.Cookie(&fiber.Cookie{Name: "authorization", Value: tokenString, SameSite: "lax", HTTPOnly: true})
+	return c.Status(fiber.StatusOK).JSON("login success")
 }

917b370c7a1c63209db2ed43bd85bcb17aac4ff4
bangsyir <bangsyir@gmail.com>
2023-01-03 18:06:50 +0800

    create post handler

---
+++ handler/post.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/bangsyir/notes/database"
+	"github.com/bangsyir/notes/models"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func CreatePost(c *fiber.Ctx) error {
+
+	post := new(models.Post)
+	if err := c.BodyParser(post); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(err.Error())
+	}
+
+	database.DB.Create(&post)
+
+	return c.Status(http.StatusCreated).JSON(post)
+}
+
+func Post(c *fiber.Ctx) error {
+	postId := c.Params("id")
+
+	type APIUser struct {
+		ID       uint
+		Name     string
+		Username string
+		Email    string
+	}
+
+	type APIpost struct {
+		ID        uint      `json:"id"`
+		Title     string    `json:"title"`
+		Desc      string    `json:"desc"`
+		CreatedAt time.Time `json:"created_at"`
+		User      APIUser   `json:"user"`
+	}
+
+	var post models.Post
+
+	database.DB.Model(&models.Post{}).Preload("User", func(db *gorm.DB) *gorm.DB {
+		return db.Select("id", "name", "username", "email")
+	}).Where("id = ?", postId).Find(&post)
+	if post.ID == 0 {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"errors": "post not found"})
+	}
+
+	data := &APIpost{
+		ID:        post.ID,
+		Title:     post.Title,
+		Desc:      post.Desc,
+		CreatedAt: post.CreatedAt,
+		User: APIUser{
+			ID:       post.User.ID,
+			Name:     post.User.Name,
+			Username: post.User.Username,
+			Email:    post.User.Email,
+		}}
+
+	return c.JSON(data)
+}

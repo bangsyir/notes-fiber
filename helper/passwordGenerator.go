98f35c06f6426aa687aa05efbe68b4e5fb664e73
bangsyir <bangsyir@gmail.com>
2023-01-04 19:38:09 +0800

    remove passwordGenerator

--- helper/passwordGenerator.go
+++
@@ -1,9 +0,0 @@
-package helper
-
-import "golang.org/x/crypto/bcrypt"
-
-func GeneratePassword(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-
-	return string(hash), err
-}
